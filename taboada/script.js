24f26a63516fe33fc03b81446fff2eafd996f210
mar7e <maria.alves.goncalves04@escola.pr.gov.br>
2025-09-12 12:13:58 -0300

    Add files via upload

---
+++ taboada/script.js
@@ -0,0 +1,19 @@
+function calcularTabuada() {
+    let numero = document.getElementById("numero").value;
+    let resultadoDiv = document.getElementById("resultado");
+
+  if (numero === "") {
+        resultadoDiv.innerHTML = "<p style='color:red'>Digite um número válido!</p>";
+  } else {
+    let resultadoHTML = "<h3>Tabuada do " + numero + "</h3>";
+    let i = 1;
+
+ while (i <= 10) {
+    resultadoHTML += "<p>" + numero + " x " + i + " = " + (numero * i) + "</p>";
+    i++;
+ }
+
+ resultadoDiv.innerHTML = resultadoHTML;
+  }
+  
+}
