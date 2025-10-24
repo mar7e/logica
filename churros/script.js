dc8327e1501fd34fcd0e491a656b07682275e3d1
mar7e <maria.alves.goncalves04@escola.pr.gov.br>
2025-10-24 11:45:01 -0300

    Add files via upload

--- churros/script.js
+++ churros/script.js
@@ -1,4 +1,9 @@
-const título = document.createElement=('div');
-título.id ='Churros Gourmet';
-título.Color ='light brown';
-document
+const título = document.createElement('h1');
+título.textContent = 'Churros e Cookies';
+document.body.appendChild(título)
+
+const imagem = document.createElement('img')
+imagem.src='/imagens/churros.jpg';
+document.body.appendChild(imagem)
+
+imagem.style.width = '400px'
